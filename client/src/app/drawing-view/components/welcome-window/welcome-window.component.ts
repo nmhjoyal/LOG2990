50d02f180f199f128c45bbf83fd0e02e49f03337
Nicole Joyal <nicole.joyal@oronospolytechnique.com>
2019-09-07 20:26:50 -0400

    NJ - Add lint check for server project, fix lint

--- client/src/app/drawing-view/components/welcome-window/welcome-window.component.ts
+++ client/src/app/drawing-view/components/welcome-window/welcome-window.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome-window',
   templateUrl: './welcome-window.component.html',
-  styleUrls: ['./welcome-window.component.scss']
+  styleUrls: ['./welcome-window.component.scss'],
 })
 export class WelcomeWindowComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    print();
+   }
 
   ngOnInit() {
+    print();
   }
 
 }
