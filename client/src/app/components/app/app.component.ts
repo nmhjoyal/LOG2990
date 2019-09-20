b0ae8dc4b88594dc0f01f7e6f6b865f05c3be6cd
Ioana <Ioana.a.bruj@gmail.com>
2019-09-20 12:07:31 -0400

    Implement basic modal window as template for new drawing + welcome window

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -3,21 +3,34 @@ import {BehaviorSubject} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {Message} from '../../../../../common/communication/message';
 import {IndexService} from '../../services/index/index.service';
+import { MatDialog } from '@angular/material';
+import { ModalWindowComponent } from 'src/app/drawing-view/components/modal-window/modal-window.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  // animations: [BrowserAnimationsModule],
 })
 export class AppComponent {
   readonly title: string = 'LOG2990';
   message = new BehaviorSubject<string>('');
 
-  constructor(private basicService: IndexService) {
+  constructor(private basicService: IndexService, public dialog: MatDialog) {
     this.basicService.basicGet()
       .pipe(
         map((message: Message) => `${message.title} ${message.body}`),
       )
       .subscribe(this.message);
+  }
+
+  openDialog(): void {
+    this.dialog.open(ModalWindowComponent, {
+      width: '800px',
+      height: '600px'
+    });
   }
 }
--- client/src/app/drawing-view/drawing-view.module.ts
+++ client/src/app/drawing-view/drawing-view.module.ts
@@ -4,13 +4,15 @@ import { ToolsComponent } from './components/tools/tools.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { WelcomeWindowComponent } from './components/welcome-window/welcome-window.component';
 import { LateralBarComponent } from './components/lateral-bar/lateral-bar.component';
+import { ModalWindowComponent } from './components/modal-window/modal-window.component';
 
 
 
 @NgModule({
-  declarations: [ToolsComponent, CanvasComponent, WelcomeWindowComponent, LateralBarComponent],
+  declarations: [ToolsComponent, CanvasComponent, WelcomeWindowComponent, LateralBarComponent, ModalWindowComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ModalWindowComponent]
 })
 export class DrawingViewModule { }
