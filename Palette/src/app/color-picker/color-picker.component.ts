6b35d30746b7d3c5aab88c4c2f3a53c11f3f4493
Jasper <jasper.ayotte-veltman@polymtl.ca>
2019-09-20 12:24:09 -0400

    add color palette

---
+++ Palette/src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.css'],
+})
+export class ColorPickerComponent {
+  public hue: string
+  public color: string
+  public alpha: string
+
+  /*setColor(color hex01: hex01){
+    color = hex;
+  }
+  setAlpha(){
+    co
+
+  }
+*/
+}
