9635e8fda1f694316750ffb11f4a56621c909b7a
Kokolokoli <oli_kokoloko@hotmail.com>
2019-09-25 01:29:33 -0400

    OCD- Added necessary structure to input colors, fill opacities and stroke opacities to the rectangles

--- client/src/app/drawing-view/components/shapes/rectangle/rectangle.component.ts
+++ client/src/app/drawing-view/components/shapes/rectangle/rectangle.component.ts
@@ -13,10 +13,13 @@ export class RectangleComponent implements OnInit {
   private _x:number = 0;
   private _y:number = 0;
   private _mouseDown: boolean = false;
-  private shiftDown: boolean = false;
+  private _shiftDown: boolean = false;
 
-  public rectangles:{x:number, y:number, 
-                      width:number, height:number}[] = [];
+  _strokeOpacity:number = 1;//These values should be loaded from the service holding color opacities
+  _fillOpacity:number = 1;
+  public rectangles:{x:number, y:number, width:number, height:number, 
+                    primeColor:string, secondColor:string
+                    strokeOpacity:number, fillOpacity:number}[] = [];
 
   constructor() { }
 
@@ -29,6 +32,7 @@ export class RectangleComponent implements OnInit {
     this._y=event.offsetY;
     this._mouseDown = true;
   }
+
   @HostListener('mouseup') onMouseUp(){
     this._mouseDown = false;
     if(!(this._rectH == 0 && this._rectW == 0))
@@ -36,7 +40,11 @@ export class RectangleComponent implements OnInit {
       {x: this._x,
        y: this._y,
        width: this._rectW,
-       height: this._rectH}
+       height: this._rectH,
+       primeColor: this.getPrimeColor(),
+       secondColor: this.getSecondColor(),
+       strokeOpacity: this._strokeOpacity,
+       fillOpacity: this._fillOpacity}
        );
     this._rectH = 0;
     this._rectW = 0;
@@ -53,7 +61,7 @@ export class RectangleComponent implements OnInit {
       this._rectW = event.offsetX - this._x;
       this._rectH = event.offsetY - this._y;
       
-      if(this.shiftDown)
+      if(this._shiftDown)
       this._rectH = this._rectW
       }
       
@@ -62,7 +70,7 @@ export class RectangleComponent implements OnInit {
   }
 
   @HostListener('keyup.shift', ['$event'])onShiftUp(event:any){
-    this.shiftDown = false;
+    this._shiftDown = false;
     //maxValue: Math.max(this._rectH, this._rectW); necessaire??
     this._rectW = this._cursorX - this._x;
     this._rectH = this._cursorY - this._y;
@@ -70,11 +78,34 @@ export class RectangleComponent implements OnInit {
     //debugger;
   }
 
-  @HostListener('keydown.shift', ['$event']) onShiftDown(event:any){
-    this.shiftDown = true;
+  @HostListener('keydown.shift', ['$event']) on_shiftDown(event:any){
+    this._shiftDown = true;
     this._rectH = this._rectW;
     
     //debugger;
+  }
+
+  setTraceMode(mode:number){
+    switch (mode) {
+      case 1://Contour
+        this._strokeOpacity = 1;//load from service
+        this._fillOpacity = 0;
+        break;
+      
+      case 2://Fill
+        this._strokeOpacity = 0;
+        this._fillOpacity = 1;//load from service
+        break;
+    
+      case 3://Contour&Fill
+          this._strokeOpacity = 1;//load from service
+          this._fillOpacity = 1;//load from service
+          break;
+
+      default:
+        break;
+    }
+
   }
 
 
@@ -102,6 +133,14 @@ export class RectangleComponent implements OnInit {
 
   getRectH(){
     return this._rectH;
+  }
+
+  getPrimeColor(){
+    return "blue"; //Place holder, should load color from service here
+  }
+
+  getSecondColor(){
+    return "green"; //Place holder, should load color from service here
   }
 
 }
