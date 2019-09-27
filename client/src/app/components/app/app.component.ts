4e22271fddc94b8606873d1ee5b3346aadf0b57f
Persia <persiashahdi@gmail.com>
2019-09-27 02:42:57 -0400

    Commented imports
    Similar to previous commented imports; unsure of necessity, likely delete

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { WelcomeWindowComponent } from 'src/app/drawing-view/components/welcome-window/welcome-window.component';
-import { LocalStorageService } from 'src/app/services/local_storage/LocalStorageService';
-import {DrawViewComponent} from '../../drawing-view/draw-view/draw-view.component';
+// import { MatDialog } from '@angular/material';
+// import { WelcomeWindowComponent } from 'src/app/drawing-view/components/welcome-window/welcome-window.component';
+// import { LocalStorageService } from 'src/app/services/local_storage/LocalStorageService';
+// import {DrawViewComponent} from '../../drawing-view/draw-view/draw-view.component';
 
 @Component({
   selector: 'app-root',
