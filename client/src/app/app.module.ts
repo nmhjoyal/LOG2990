a637e73a54725be09447c01f400caf3e8c5bcff4
Kokolokoli <oli_kokoloko@hotmail.com>
2019-09-30 16:29:29 -0400

    OCD- correctly imported the drawingview module into appmodule, except for the entrycomponents

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,22 +8,24 @@ import {MatInputModule} from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './components/app/app.component';
-import { MockCanvasComponent } from './drawing-view/components/mock-canvas/mock-canvas.component';
-import { ModalWindowComponent } from './drawing-view/components/modal-window/modal-window.component';
-import { NewDrawingWindowComponent } from './drawing-view/components/new-drawing-window/new-drawing-window.component';
-import { WelcomeWindowComponent } from './drawing-view/components/welcome-window/welcome-window.component';
 import { LocalStorageService } from './services/local_storage/LocalStorageService';
+import { ModalWindowComponent } from './drawing-view/components/modal-window/modal-window.component';
+import { DrawingViewModule } from './drawing-view/drawing-view.module'
+import { WelcomeWindowComponent } from './drawing-view/components/welcome-window/welcome-window.component';
+import { NewDrawingWindowComponent } from './drawing-view/components/new-drawing-window/new-drawing-window.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewDrawingWindowComponent,
-    MockCanvasComponent,
-    WelcomeWindowComponent,
     ModalWindowComponent as Type<ModalWindowComponent>,
+    WelcomeWindowComponent,
+    NewDrawingWindowComponent,
+
   ],
   imports: [
     BrowserModule,
+    DrawingViewModule,
     HttpClientModule,
     MatListModule,
     MatToolbarModule,
--- client/src/app/drawing-view/drawing-view.module.ts
+++ client/src/app/drawing-view/drawing-view.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -9,26 +8,24 @@ import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from '../components/app/app.component';
-<<<<<<< HEAD
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { LateralBarComponent } from './components/lateral-bar/lateral-bar.component';
 import { MockCanvasComponent } from './components/mock-canvas/mock-canvas.component';
 import { RectangleComponent } from './components/tools/shapes/rectangle/rectangle.component';
 import { ShapeToolboxComponent } from './components/tools/shapes/shape-toolbox/shape-toolbox.component';
 import { ToolsComponent } from './components/tools/tools.component';
-import { WelcomeWindowComponent } from './components/welcome-window/welcome-window.component';
 
 @NgModule({
-  declarations: [AppComponent,
+  declarations: [
     ToolsComponent,
     CanvasComponent,
-    WelcomeWindowComponent,
     LateralBarComponent,
-    RectangleComponent, 
-    ShapeToolboxComponent, 
-    MockCanvasComponent,],
-    
+    RectangleComponent,
+    ShapeToolboxComponent,
+    MockCanvasComponent, ],
+
+  exports: [MockCanvasComponent],
+  
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -41,6 +38,7 @@ import { WelcomeWindowComponent } from './components/welcome-window/welcome-wind
   ],
   providers: [{ provide: MatDialogRef, useValue: {} },
   { provide: MAT_DIALOG_DATA, useValue: [] }, ],
-  bootstrap: [AppComponent],
+  bootstrap: [MockCanvasComponent],
+  entryComponents: [],
 })
 export class DrawingViewModule { }
