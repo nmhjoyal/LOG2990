441f4c560fc071828452bbdd817b4a884fcc3c8b
Nicole Joyal <nicole.joyal@oronospolytechnique.com>
2019-10-04 16:23:01 -0400

    NJ - Rename directory to canvas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,7 @@ import {MatInputModule} from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './components/app/app.component';
-import { CanvasComponent } from './drawing-view/components/mock-canvas/canvas.component';
+import { CanvasComponent } from './drawing-view/components/canvas/canvas.component';
 import { ModalWindowComponent } from './drawing-view/components/modal-window/modal-window.component';
 import { NewDrawingWindowComponent } from './drawing-view/components/new-drawing-window/new-drawing-window.component';
 import { WelcomeWindowComponent } from './drawing-view/components/welcome-window/welcome-window.component';
--- client/src/app/drawing-view/drawing-view.module.ts
+++ client/src/app/drawing-view/drawing-view.module.ts
@@ -8,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from '../components/app/app.component';
-import { CanvasComponent } from './components/mock-canvas/canvas.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
 
 @NgModule({
   declarations: [
