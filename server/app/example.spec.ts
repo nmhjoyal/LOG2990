4664df19a2db654c8c0dab638bef096c70601f8d
Ioana <ioana.bruj@polymtl.ca>
2019-10-06 15:00:00 -0400

    IB - Fix TSlint in common, commit new package.json, yarn.lock, fix errors in server spec file to work with common tslint

--- server/app/example.spec.ts
+++ server/app/example.spec.ts
@@ -1,5 +1,3 @@
-// tslint:disable:no-magic-numbers
-// tslint:disable:no-console
 import {assert, expect} from 'chai';
 
 describe('A starting set of test examples to showcase test-suite behaviour', () => {
@@ -30,7 +28,7 @@ describe('A starting set of test examples to showcase test-suite behaviour', () 
             // Get a promise of a value that will be later resolved
             const promiseOfValue =  promiseGenerator().next();
             // Assign a value to our object
-            promiseOfValue.value.then((v) => {
+            promiseOfValue.value.then((v: number) => {
                 variableNeedingToBeChangedEachRun = v;
                 d();
             });
