981d9ed7f732e8920cac43e64a1d9f1c2c1a5d0c
emleba <emile.leblanc@live.com>
2019-10-08 20:22:09 -0400

    implementing filters

--- client/src/app/drawing-view/drawing-view.module.ts
+++ client/src/app/drawing-view/drawing-view.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CanvasComponent } from './components/canvas/canvas.component';
 import { LateralBarComponent } from './components/lateral-bar/lateral-bar.component';
 import { MockCanvasComponent } from './components/mock-canvas/mock-canvas.component';
--- client/src/app/services/local_storage/LocalStorageService.ts
+++ client/src/app/services/local_storage/LocalStorageService.ts
@@ -1,41 +1,13 @@
-import { Inject, Injectable } from '@angular/core';
-import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { Injectable, Inject } from '@angular/core';
+import { StorageService, LOCAL_STORAGE } from 'ngx-webstorage-service';
 
 const WELCOME_WINDOW_KEY = 'showAgain';
 
 @Injectable()
 export class LocalStorageService {
 
-    // Shape Handling attributes
-    private rectangleSelected = false;
-    private crayonSelected = false;
-    private paintbrushSelected = false;
-
-    // Color service simulating attributes
-    primaryColor = 'green';
-    secondaryColor = 'rgb(76, 24, 199)';
-
-    // Shape Storage
-    rectangles: {x: number, y: number, width: number, height: number,
-        primeColor: string, secondColor: string
-        strokeOpacity: number, strokeWidth: number, fillOpacity: number}[] = [];
-
-    lines: {points: string, color: string, strokeWidth: number,
-        fill: string, strokeLinecap: string, filter: string}[] = [];
-
-    paints: {points: string, color: string, strokeWidth: number,
-            fill: string, strokeLinecap: string,
-            filter: {baseFrequency: string, numOctaves: string, scale: string}}[] = [];
-
-    filters: {baseFrequency: string, numOctaves: string, scale: string}[] =
-    [{baseFrequency: '0.2', numOctaves: '2', scale: '5'},
-     {baseFrequency: '0.4', numOctaves: '6', scale: '3'},
-     {baseFrequency: '0.6', numOctaves: '4', scale: '4'},
-     {baseFrequency: '1.0', numOctaves: '5', scale: '3'},
-     {baseFrequency: '0.9', numOctaves: '3', scale: '6'}];
-
     constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) {
-
+        // empty body
     }
 
     setShowAgain(showAgain: boolean): void {
@@ -48,73 +20,6 @@ export class LocalStorageService {
         }
         this.storage.set(WELCOME_WINDOW_KEY, true);
         return true;
-    }
-
-    // Tool Handling methods
-    clear(): void {
-        this.reset();
-        this.rectangles.length = 0;
-        this.lines.length = 0;
-        this.paints.length=0;
-    }
-
-    reset(): void {
-        this.rectangleSelected = false;
-        this.crayonSelected = false;
-        this.paintbrushSelected = false;
-    }
-
-    chooseRectangle(): void {
-        this.reset();
-        this.rectangleSelected = true;
-    }
-
-    chooseCrayon(): void {
-        this.reset();
-        this.crayonSelected = true;
-    }
-
-    choosePaintbrush(): void {
-        this.reset();
-        this.paintbrushSelected = true;
-    }
-
-    chooseOther(): void {
-        this.reset();
-    }
-
-    get isRectangle(): boolean {
-        return this.rectangleSelected;
-    }
-
-    get isCrayon(): boolean {
-        return this.crayonSelected;
-    }
-
-    get isPinceau(): boolean {
-        return this.paintbrushSelected;
-    }
-
-    // Color service simulating (DELETE ONCE IMPLEMENTED WITH COLOR SERVICE)
-    switchColor() {
-        this.primaryColor = 'rgb('
-                              + Math.floor(Math.random() * 255)
-                              + ',' + Math.floor(Math.random() * 255)
-                              + ','  + Math.floor(Math.random() * 255)
-                              + ')';
-        this.secondaryColor = 'rgb('
-                                + Math.floor(Math.random() * 255)
-                                + ',' + Math.floor(Math.random() * 255)
-                                + ','  + Math.floor(Math.random() * 255)
-                                + ')';
-    }
-
-    get PrimaryColor(): string {
-        return this.primaryColor;
-    }
-
-    get SecondColor(): string {
-        return this.secondaryColor;
     }
 
 }
