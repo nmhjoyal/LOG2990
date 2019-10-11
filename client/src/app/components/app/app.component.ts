ac5b3d756e0b6d6cb03edeca36f71d30d2532fb9
Nicole Joyal <nicole.joyal@oronospolytechnique.com>
2019-10-11 13:39:43 -0400

    NJ - Merge with dev

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
-import { ModalWindowComponent } from 'src/app/drawing-view/components/modal-window/modal-window.component';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { NewDrawingWindowComponent } from 'src/app/drawing-view/components/new-drawing-window/new-drawing-window.component';
 import { NewDrawingModalData } from 'src/app/drawing-view/components/NewDrawingModalData';
 import { WelcomeWindowComponent } from 'src/app/drawing-view/components/welcome-window/welcome-window.component';
@@ -14,17 +13,20 @@ import { AppConstants } from 'src/AppConstants';
 })
 export class AppComponent implements OnInit {
 
+  constructor(private dialog: MatDialog, private storage: LocalStorageService, @Inject(MAT_DIALOG_DATA) private data: NewDrawingModalData) {
+    this.data.drawingHeight = window.innerHeight - AppConstants.TITLEBAR_WIDTH;
+    this.data.drawingWidth = window.innerWidth - AppConstants.SIDEBAR_WIDTH;
+    this.data.drawingColor = AppConstants.WHITE_HEX;
+    this.data.canvasIsDrawnOn = true;
+  }
+
+  ngOnInit(): void {
+    this.openWelcomeScreen();
+  }
+
   @HostListener('document:keydown.control.o', ['$event']) onKeydownHandler(event: KeyboardEvent) {
     event.preventDefault();
     this.confirmNewDrawing();
-  }
-
-  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<ModalWindowComponent>,
-              private storage: LocalStorageService, @Inject(MAT_DIALOG_DATA) public data: NewDrawingModalData) {
-    this.data.drawingHeight = window.innerHeight - AppConstants.TITLEBAR_WIDTH;
-    this.data.drawingWidth = window.innerWidth - AppConstants.SIDEBAR_WIDTH;
-    this.data.drawingColor = '#ffffff';
-    this.data.canvasIsDrawnOn = true;
   }
 
   confirmNewDrawing(): void {
@@ -42,10 +44,6 @@ export class AppComponent implements OnInit {
       data: NewDrawingWindowComponent.prototype.data,
       panelClass: 'new-drawing-window',
     });
-  }
-
-  ngOnInit(): void {
-    this.openWelcomeScreen();
   }
 
   openWelcomeScreen(): void {
--- client/src/app/drawing-view/drawing-view.module.ts
+++ client/src/app/drawing-view/drawing-view.module.ts
@@ -8,14 +8,19 @@ import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from '../components/app/app.component';
-import { MockCanvasComponent } from './components/mock-canvas/mock-canvas.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MockCanvasComponent,
-  ],
   imports: [
+    BrowserModule,
+    HttpClientModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  exports: [
     BrowserModule,
     HttpClientModule,
     MatDialogModule,
