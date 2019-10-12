eb1d94448a23bd72f03425f515072fe3ad9a1113
Ioana <ioana.bruj@polymtl.ca>
2019-10-12 18:44:43 -0400

    IB - Fix duplicate identifier left over from old INewDrawingModalData file location

--- client/src/app/drawing-view/components/canvas/canvas.component.spec.ts
+++ client/src/app/drawing-view/components/canvas/canvas.component.spec.ts
@@ -11,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from 'src/app/components/app/app.component';
 import { LocalStorageService } from 'src/app/services/local_storage/LocalStorageService';
 import { DrawingViewModule } from '../../drawing-view.module';
-import { INewDrawingModalData } from '../INewDrawingModalData';
 import { ModalWindowComponent } from '../modal-window/modal-window.component';
 import { INewDrawingModalData } from '../new-drawing-window/INewDrawingModalData';
 import { NewDrawingWindowComponent } from '../new-drawing-window/new-drawing-window.component';
