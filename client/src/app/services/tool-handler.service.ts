2e362574d395524d61ffdfbeab1f12d757a9c54c
emleba <emile.leblanc@live.com>
2019-10-13 15:35:39 -0400

    EL- created pipette tool and added tool-handler service

---
+++ client/src/app/services/tool-handler.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import { IDrawingTool } from 'src/app/drawing-view/components/tools/assets/interfaces/drawing-tool-interface';
+import { ToolConstants } from 'src/app/drawing-view/components/tools/assets/tool-constants';
+import { AppConstants } from 'src/AppConstants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToolHandlerService {
+
+// Shape Handling attributes
+noneSelected: boolean;
+rectangleSelected: boolean;
+colourApplicatorSelected: boolean;
+crayonSelected: boolean;
+paintbrushSelected: boolean;
+pipetteSelected: boolean;
+​
+// Color service simulating attributes
+primaryColor: string;
+secondaryColor: string;
+​
+// drawings Storage
+drawings: IDrawingTool[] = []; // change type to parent of IDrawingTool
+
+// Attributes Storage
+crayonStrokeWidth: number;
+paintbrushStrokeWidth: number;
+paintbrushFilter: string;
+
+constructor() {
+    this.noneSelected = true;
+    this.rectangleSelected = false;
+    this.colourApplicatorSelected = false;
+    this.crayonSelected = false;
+    this.paintbrushSelected = false;
+    this.pipetteSelected = false;
+    this.primaryColor = AppConstants.DEFAULT_PRIMARY_COLOUR;
+    this.secondaryColor = AppConstants.DEFAULT_SECONDARY_COLOUR;
+    this.crayonStrokeWidth = ToolConstants.DEFAULT_STROKE_WIDTH;
+    this.paintbrushStrokeWidth = ToolConstants.DEFAULT_STROKE_WIDTH;
+    this.paintbrushFilter = ToolConstants.NONE;
+}
+​
+// Tool Handling methods
+clearPage(): void {
+    this.resetSelection();
+    this.drawings.length = 0;
+}
+​
+resetSelection(): void {
+    this.noneSelected = true;
+    this.rectangleSelected = false;
+    this.colourApplicatorSelected = false;
+    this.crayonSelected = false;
+    this.paintbrushSelected = false;
+    this.pipetteSelected = false;
+}
+​
+chooseRectangle(): void {
+    this.resetSelection();
+    this.rectangleSelected = true;
+    this.noneSelected = false;
+}
+​
+chooseColourApplicator(primaryColor: string, secondaryColor: string): void {
+    this.resetSelection();
+    this.primaryColor = primaryColor;
+    this.secondaryColor = secondaryColor;
+    this.colourApplicatorSelected = true;
+    this.noneSelected = false;
+}
+​
+chooseCrayon(): void {
+    this.resetSelection();
+    this.crayonSelected = true;
+    this.noneSelected = false;
+}
+​
+choosePaintbrush(): void {
+    this.resetSelection();
+    this.paintbrushSelected = true;
+    this.noneSelected = false;
+}
+
+choosePipette(): void {
+  this.resetSelection();
+  this.pipetteSelected = true;
+  this.noneSelected = false;
+}
+​
+chooseOther(): void {// Place holder for unimplemented tools
+    this.resetSelection();
+}
+​
+}
