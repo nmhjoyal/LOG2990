22f3d3941299da5ff0684d37594952f0ae1a6cc0
Kokolokoli <oli_kokoloko@hotmail.com>
2019-10-14 17:49:26 -0400

    OCD - fixed access to undefined error in shape-toolbox tests

--- client/src/app/drawing-view/components/tools/shapes/shape-toolbox/shape-toolbox.component.spec.ts
+++ client/src/app/drawing-view/components/tools/shapes/shape-toolbox/shape-toolbox.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShapeToolboxComponent } from './shape-toolbox.component';
+import { CanvasComponent } from '../../../canvas/canvas.component';
 
 describe('ShapeToolboxComponent', () => {
   let component: ShapeToolboxComponent;
+  const mockCanvasComponent: jasmine.SpyObj<CanvasComponent> = jasmine.createSpyObj('CanvasComponent', ['activeTool']);
   let fixture: ComponentFixture<ShapeToolboxComponent>;
 
   beforeEach(async(() => {
@@ -16,6 +18,7 @@ describe('ShapeToolboxComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ShapeToolboxComponent);
     component = fixture.componentInstance;
+    component.canvas = mockCanvasComponent;
     fixture.detectChanges();
   });
 
@@ -23,7 +26,7 @@ describe('ShapeToolboxComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should return the appropriate component', () => {
+  it('#getComponent should return the appropriate component', () => {
     const returned = component.getComponent();
     expect(returned).toEqual(component.canvas.activeTool);
   });
