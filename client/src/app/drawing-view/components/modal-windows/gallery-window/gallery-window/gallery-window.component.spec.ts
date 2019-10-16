9a784537785f1f6843364be9fae7d5bf81a9b2c7
Ioana <ioana.bruj@polymtl.ca>
2019-10-15 20:59:25 -0400

    IB - Create GalleryWindowComponent

---
+++ client/src/app/drawing-view/components/modal-windows/gallery-window/gallery-window/gallery-window.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GalleryWindowComponent } from './gallery-window.component';
+
+describe('GalleryWindowComponent', () => {
+  let component: GalleryWindowComponent;
+  let fixture: ComponentFixture<GalleryWindowComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GalleryWindowComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GalleryWindowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/drawing-view/components/modal-windows/gallery-window/gallery-window/gallery-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-window',
+  templateUrl: './gallery-window.component.html',
+  styleUrls: ['./gallery-window.component.scss']
+})
+export class GalleryWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
