2e9ebd9b00213525030eb91fcab2acbec8f34996
emleba <emile.leblanc@live.com>
2019-10-18 18:04:43 -0400

    EL- fixed crayon and paintbrush tests

--- client/src/app/drawing-view/components/tools/drawing-tools/crayon/crayon.component.spec.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/crayon/crayon.component.spec.ts
@@ -1,83 +1,66 @@
     // tslint:disable: no-string-literal
-    import { HttpClientModule } from '@angular/common/http';
-    import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-    import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-    import { DrawingViewModule } from 'src/app/drawing-view/drawing-view.module';
-    import { ToolHandlerService } from 'src/app/services/tool-handler/tool-handler.service';
-    import { CrayonComponent } from './crayon.component';
-    import { OnInit, OnDestroy } from '@angular/core';
-    import { AttributesService } from '../../assets/attributes/attributes.service';
-    import { StrokeAbstract } from '../../assets/abstracts/stroke-abstract/stroke-abstract';
-import { ColorService } from 'src/app/services/color_service/color.service';
-    
-    class AbstractSpy extends StrokeAbstract implements OnInit, OnDestroy {
-    
-      constructor(serviceInstance: ToolHandlerService, attributesInstance: AttributesService, colorInstance: ColorService) {
-        super(serviceInstance, attributesInstance, colorInstance);
-      }
-      // mock of abstract methods
-      ngOnInit(): void {
-      // empty block
-      }
-      ngOnDestroy(): void {
-          // empty block
-      }
-      saveShape(): void {
-        // empty block
-      }
-      saveAttribute(): void{
-        // empty block
-      }
-    }
-    
-    describe('CrayonComponent', () => {
-      let component: CrayonComponent;
-      let fixture: ComponentFixture<CrayonComponent>;
-      let toolSpy: ToolHandlerService;
-      let attributeSpy: AttributesService;
-      let colorSpy: ColorService;
-      let abstractSpy: AbstractSpy;
-    
-      beforeEach(async(() => {
-        TestBed.configureTestingModule({
-          declarations: [
-          ],
-          imports: [
-            HttpClientModule,
-            BrowserAnimationsModule,
-            DrawingViewModule,
-          ],
-        })
-          .compileComponents();
-      }));
-    
-      beforeEach( async () => {
-        fixture = TestBed.createComponent(CrayonComponent);
-        colorSpy = new ColorService();
-        toolSpy = new ToolHandlerService(colorSpy);
-        attributeSpy = new AttributesService();
-        component = new CrayonComponent(toolSpy, attributeSpy, colorSpy);
-        fixture.detectChanges();
-        abstractSpy = new AbstractSpy(toolSpy, attributeSpy, colorSpy);
-      });
-    
-      it('should create', () => {
-        expect(component).toBeTruthy();
-      });
-    
-      it('should save the attribute when the component is destroyed', () => {
-        const spy = spyOn(component, 'saveAttribute');
-        component.ngOnDestroy();
-        expect(spy).toHaveBeenCalled();
-      });
-    
-      it('should properly save the attributes', () => {
-        abstractSpy['stroke'].strokeWidth = 3;
-        abstractSpy.saveAttribute();
-        expect(attributeSpy.paintbrushAttributes.savedStrokeWidth).toEqual(abstractSpy['stroke'].strokeWidth);
-      });
-    
-      
-    
-    });
-    
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolHandlerService } from 'src/app/services/tool-handler/tool-handler.service';
+import { ToolConstants } from '../../assets/tool-constants';
+import { AttributesService } from '../../assets/attributes/attributes.service';
+import { CrayonComponent } from './crayon.component';
+
+const INITIAL_X = 150;
+const INITIAL_Y = 200;
+
+
+describe('CrayonComponent', () => {
+  let component: CrayonComponent;
+  let attrService: AttributesService;
+  let fixture: ComponentFixture<CrayonComponent>;
+  const toolServiceMock: jasmine.SpyObj<ToolHandlerService> = jasmine.createSpyObj('ToolHandlerService', ['']);
+  const attributesServiceMock: AttributesService = new AttributesService();
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CrayonComponent],
+      imports: [BrowserAnimationsModule],
+      providers: [
+        { provide: ToolHandlerService, useValue: toolServiceMock, },
+        { provide: AttributesService, useValue: attributesServiceMock, },
+      ],
+    })
+      .compileComponents();
+  }));
+    // tslint:disable:no-string-literal
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CrayonComponent);
+    attrService = TestBed.get(AttributesService);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component['x'] = INITIAL_X;
+    component['y'] = INITIAL_Y;
+    component['mouseDown'] = true;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    attrService.resetSavedAttributes();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should save the attribute when the component is destroyed', () => {
+    const spy = spyOn(component, 'saveAttribute');
+    component.ngOnDestroy();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should properly save the attributes', () => {
+    component.ngOnDestroy();
+    expect(attrService.crayonAttributes.savedStrokeWidth).toEqual(ToolConstants.DEFAULT_STROKE_WIDTH,
+      'stroke.strokeWidth was not successfully saved upon destruction');
+    expect(attrService.crayonAttributes.wasSaved).toBe(true, '#ngOnDestroy did not set wasSaved to true');
+
+  });
+});
--- client/src/app/drawing-view/components/tools/drawing-tools/paintbrush/paintbrush.component.spec.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/paintbrush/paintbrush.component.spec.ts
@@ -1,65 +1,49 @@
     // tslint:disable: no-string-literal
-import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DrawingViewModule } from 'src/app/drawing-view/drawing-view.module';
 import { ToolHandlerService } from 'src/app/services/tool-handler/tool-handler.service';
 import { ToolConstants } from '../../assets/tool-constants';
 import { PaintbrushComponent } from './paintbrush.component';
-import { OnInit, OnDestroy } from '@angular/core';
 import { AttributesService } from '../../assets/attributes/attributes.service';
-import { StrokeAbstract } from '../../assets/abstracts/stroke-abstract/stroke-abstract';
-import { ColorService } from 'src/app/services/color_service/color.service';
 
-class AbstractSpy extends StrokeAbstract implements OnInit, OnDestroy {
+const INITIAL_X = 150;
+const INITIAL_Y = 200;
 
-  constructor(serviceInstance: ToolHandlerService, attributesInstance: AttributesService, colorInstance: ColorService) {
-    super(serviceInstance, attributesInstance, colorInstance);
-  }
-  // mock of abstract methods
-  ngOnInit(): void {
-  // empty block
-  }
-  ngOnDestroy(): void {
-      // empty block
-  }
-  saveShape(): void {
-    // empty block
-  }
-  saveAttribute(): void{
-    // empty block
-  }
-}
 
 describe('PaintbrushComponent', () => {
   let component: PaintbrushComponent;
+  let attrService: AttributesService;
   let fixture: ComponentFixture<PaintbrushComponent>;
-  let toolSpy: ToolHandlerService;
-  let attributeSpy: AttributesService;
-  let colorSpy: ColorService;
-  let abstractSpy: AbstractSpy;
+  const toolServiceMock: jasmine.SpyObj<ToolHandlerService> = jasmine.createSpyObj('ToolHandlerService', ['']);
+  const attributesServiceMock: AttributesService = new AttributesService();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-      ],
-      imports: [
-        HttpClientModule,
-        BrowserAnimationsModule,
-        DrawingViewModule,
+      declarations: [PaintbrushComponent],
+      imports: [BrowserAnimationsModule],
+      providers: [
+        { provide: ToolHandlerService, useValue: toolServiceMock, },
+        { provide: AttributesService, useValue: attributesServiceMock, },
       ],
     })
       .compileComponents();
   }));
+    // tslint:disable:no-string-literal
 
-  beforeEach( async () => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(PaintbrushComponent);
-    colorSpy = new ColorService();
-    toolSpy = new ToolHandlerService(colorSpy);
-    attributeSpy = new AttributesService();
-    component = new PaintbrushComponent(toolSpy, attributeSpy, colorSpy);
+    attrService = TestBed.get(AttributesService);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    abstractSpy = new AbstractSpy(toolSpy, attributeSpy, colorSpy);
+
+    component['x'] = INITIAL_X;
+    component['y'] = INITIAL_Y;
+    component['mouseDown'] = true;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    attrService.resetSavedAttributes();
   });
 
   it('should create', () => {
@@ -73,51 +57,53 @@ describe('PaintbrushComponent', () => {
   });
 
   it('should properly save the attributes', () => {
-    abstractSpy['stroke'].strokeWidth = 3;
-    abstractSpy['stroke'].filter = ToolConstants.FILTER_ID.FILTER1;
-    abstractSpy.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedStrokeWidth).toEqual(abstractSpy['stroke'].strokeWidth);
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(abstractSpy['stroke'].filter);
+    component.ngOnDestroy();
+    expect(attrService.paintbrushAttributes.savedStrokeWidth).toEqual(ToolConstants.DEFAULT_STROKE_WIDTH,
+      'stroke.strokeWidth was not successfully saved upon destruction');
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER0,
+      'the filter was not successfully saved upon destruction');
+    expect(attrService.paintbrushAttributes.wasSaved).toBe(true, '#ngOnDestroy did not set wasSaved to true');
+
   });
 
   it('should set no filter for case 0', () => {
     component.setFilter(0);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER0);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER0);
   });
 
   it('should set the appropriate filter for the first case', () => {
     component.setFilter(1);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER1);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER1);
   });
 
   it('should set the appropriate filter for the second case', () => {
     component.setFilter(2);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER2);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER2);
   });
 
   it('should set the appropriate filter for the third case', () => {
     component.setFilter(3);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER3);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER3);
   });
 
   it('should set the appropriate filter for the fourth case', () => {
     component.setFilter(4);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER4);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER4);
   });
   it('should set the appropriate filter for the fifth case', () => {
     component.setFilter(5);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER5);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER5);
   });
   it('should default to no filter', () => {
     component.setFilter(10);
     component.saveAttribute();
-    expect(attributeSpy.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER0);
+    expect(attrService.paintbrushAttributes.savedFilter).toEqual(ToolConstants.FILTER_ID.FILTER0);
   });
 
 });
