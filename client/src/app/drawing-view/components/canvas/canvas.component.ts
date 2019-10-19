18baf1d433c50bd11b6236dfcc30da5722c33308
Nicole Joyal <nicole.joyal@oronospolytechnique.com>
2019-10-19 15:23:33 -0400

    NJ - Reimplement colour applicator

--- client/src/app/drawing-view/components/canvas/canvas.component.ts
+++ client/src/app/drawing-view/components/canvas/canvas.component.ts
@@ -4,6 +4,8 @@ import { ColorService } from 'src/app/services/color_service/color.service';
 import { ToolHandlerService } from 'src/app/services/tool-handler/tool-handler.service';
 import { INewDrawingModalData } from '../new-drawing-window/INewDrawingModalData';
 import { ToolAbstract } from '../tools/assets/abstracts/tool-abstract/tool-abstract';
+import { IDrawingTool } from '../tools/assets/interfaces/drawing-tool-interface';
+import { IShape } from '../tools/assets/interfaces/shape-interface';
 import { Id } from '../tools/assets/tool-constants';
 
 @Component({
@@ -18,6 +20,31 @@ export class CanvasComponent {
 
   constructor(@Inject(MAT_DIALOG_DATA) protected data: INewDrawingModalData,
               public toolHandler: ToolHandlerService, public colorService: ColorService) {
+  }
+
+  applyColourToCanvas() {
+    if (this.toolHandler.colourApplicatorSelected) {
+      this.data.drawingColor = this.colorService.color[0];
+    }
+  }
+
+  applyColourToLine(line: IDrawingTool): void {
+    if (this.toolHandler.colourApplicatorSelected) {
+      line.color = this.colorService.color[0];
+    }
+  }
+
+  applyColourToShape(shape: IShape): void {
+    if (this.toolHandler.colourApplicatorSelected && shape.primaryColor !== 'none') {
+      shape.primaryColor = this.colorService.color[0];
+    }
+  }
+
+  applySecondaryColourToShape(event: MouseEvent, shape: IShape): void {
+    event.preventDefault();
+    if (this.toolHandler.colourApplicatorSelected && shape.secondaryColor !== 'none') {
+      shape.secondaryColor = this.colorService.color[1];
+    }
   }
 
 }
