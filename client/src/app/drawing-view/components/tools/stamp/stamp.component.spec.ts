d1ebf3dd5d3fc06e302f142d8161c09cbc0ca364
Kokolokoli <oli_kokoloko@hotmail.com>
2019-10-22 15:17:15 -0400

    OCD - started tests for stamp

---
+++ client/src/app/drawing-view/components/tools/stamp/stamp.component.spec.ts
@@ -0,0 +1,28 @@
+// tslint:disable: no-string-literal
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ToolHandlerService } from 'src/app/services/tool-handler/tool-handler.service';
+import { AttributesService } from '../assets/attributes/attributes.service';
+import { StampComponent } from './stamp.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+
+describe('StampComponent', () => {
+    let component: StampComponent;
+    let attrService: AttributesService;
+    let fixture: ComponentFixture<StampComponent>;
+    const toolServiceMock: jasmine.SpyObj<ToolHandlerService> = jasmine.createSpyObj('ToolHandlerService', ['']);
+    const attributesServiceMock: AttributesService = new AttributesService();
+
+    beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        declarations: [StampComponent],
+        imports: [BrowserDynamicTestingModule],
+        providers: [
+        { provide: ToolHandlerService, useValue: toolServiceMock, },
+        { provide: AttributesService, useValue: attributesServiceMock, },
+        ],
+    })
+        .compileComponents();
+    }));
+
+
+});
