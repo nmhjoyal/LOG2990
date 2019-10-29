9a3de9c6d64cc78693fbf53664e0628eead1b174
Persia <persiashahdi@gmail.com>
2019-10-29 18:10:39 -0400

    PS - added two tests for verifying state of leftClicked bool variable

--- client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.spec.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ColorService } from '../../../../../services/color_service/color.service';
+import { SelectorService } from '../../../../../services/selector-service/selector-service';
+import { ToolHandlerService } from '../../../../../services/tool-handler/tool-handler.service';
+import { IShape } from 'src/app/drawing-view/components/tools/assets/interfaces/shape-interface';
 import { EraserComponent } from './eraser.component';
 
 describe('EraserComponent', () => {
   let component: EraserComponent;
   let fixture: ComponentFixture<EraserComponent>;
+  let toolHandlerservice: ToolHandlerService;
+  let colorservice: ColorService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EraserComponent ]
+      declarations: [ EraserComponent ],
+      providers: [SelectorService]
     })
     .compileComponents();
   }));
@@ -22,4 +28,36 @@ describe('EraserComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set leftClicked to true when mouse is clicked', () => {
+    const event = new MouseEvent('onmousedown');
+    component.mouseDown(event);
+    expect(component.leftClicked).toBeTruthy();
+  });
+
+  it('should set leftClicked to false when mouseup', () => {
+    component.mouseUp();
+    expect(component.leftClicked).toBeFalsy();
+  });
+
+  // it('should delete a drawing', () => {
+  //   colorservice = new ColorService();
+  //   toolHandlerservice = new ToolHandlerService(colorservice);
+  //   const mydrawing: IShape = {
+  //     id: 'myshape',
+  //     x: 50,
+  //     y: 50,
+  //     width: 50,
+  //     height: 50,
+  //     verticesNumber: 4,
+  //     vertices: '',
+  //     primaryColor: 'someColor',
+  //     secondaryColor: 'someColor',
+  //     strokeOpacity: 1,
+  //     strokeWidth: 1,
+  //     fillOpacity: 1,
+  //   };
+  //   toolHandlerservice.drawings.push(mydrawing);
+
+  // })
 });
