1c87e788eefcb244ebd8213e593b7a066afaed74
Persia <persiashahdi@gmail.com>
2019-10-28 21:37:45 -0400

    PS - added ngIf for eraser in canvas.component.html; added a MouseDown method to delete drawings

--- client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import ClickHelper from '../../../../../helpers/click-helper/click-helper';
+import { SelectorService } from '../../../../../services/selector-service/selector-service';
+import { ToolHandlerService } from '../../../../../services/tool-handler/tool-handler.service';
 
 @Component({
   selector: 'app-eraser',
   templateUrl: './eraser.component.html',
-  styleUrls: ['./eraser.component.scss']
+  styleUrls: ['./eraser.component.scss'],
 })
 export class EraserComponent {
 
-  constructor(private clickHelper: ClickHelper) { }
+  constructor(private toolService: ToolHandlerService, private selectorService: SelectorService) {
+  }
+
+  @Input() windowHeight: number;
+  @Input() windowWidth: number;
+
+  @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent): void {
+    for (const drawing of this.toolService.drawings) {
+      if (this.selectorService.cursorTouchesObject(drawing, ClickHelper.getXPosition(event), ClickHelper.getYPosition(event))) {
+        const drawingIndex = this.toolService.drawings.indexOf(drawing);
+        this.toolService.drawings.splice(drawingIndex, 1);
+      }
+    }
+
+  }
 
 }
