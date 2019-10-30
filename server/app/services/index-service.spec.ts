730b46317d42f3c3d6cb48d5a4641390c3e3550f
Ioana <ioana.bruj@polymtl.ca>
2019-10-30 14:31:07 -0400

    IB - fix server tests crashing pipeline

--- server/app/services/index-service.spec.ts
+++ server/app/services/index-service.spec.ts
@@ -1,18 +1,11 @@
 // tslint:disable: no-implicit-dependencies
 // tslint:disable: no-require-imports
 // tslint:disable: no-var-requires
-import { IDrawing } from '../../../common/drawing-information/IDrawing';
-import { ITag } from '../../../common/drawing-information/ITag';
-import { IndexService } from '../services/index.service';
 const server = require('../server');
-const assert = require('assert');
-const http = require('http');
-const chai = require('chai');
+let chaiImport = require('chai');
 
 describe('IndexService', () => {
-    let service: IndexService;
-    const { expect } = chai;
-    const should = require('chai').should();
+    const { expect } = chaiImport;
 
     describe('server', () => {
         before(() => {
@@ -24,58 +17,28 @@ describe('IndexService', () => {
         });
     });
 
-    /* Test */
-
-    it('#savedrawing should return true if drawing is valid', function(done) {
-        const drawingToSave: IDrawing = new IDrawing();
-        const attribute = this.saveDrawing(drawingToSave);
-        expect(attribute).to.return(true);
-        done();
-    });
-
-    it('#savedrawing should return false if drawing is invalid', function(done) {
-        const drawingToSave: IDrawing = new IDrawing();
-        const attribute = this.saveDrawing(drawingToSave);
-        expect(attribute).to.return(false);
-        done();
-    });
-
-    it('#saveTag should return true if tag is valid', function(done) {
-        const drawingToSave: ITag = new ITag();
-        const attribute = this.saveTag(drawingToSave);
-        expect(attribute).to.return(true);
-        done();
-    });
-
-    it('#savetag should return false if tag is invalid', function(done) {
-        const drawingToSave: ITag = new ITag();
-        const attribute = this.saveTag(drawingToSave);
-        expect(attribute).to.return(false);
-        done();
-    });
-
     it('#getDrawings should return drawings', function(done) {
         const attribute = this.getDrawings();
-        expect(attribute).to.return(attribute);
+        expect(attribute).to.equal(attribute);
         done();
     });
 
     it('#getDrawing should return drawing', function(done) {
         const drawingTimestampID = '';
         const attribute = this.getDrawing(drawingTimestampID);
-        expect(this.getDrawing(drawingTimestampID)).to.return(attribute);
+        expect(this.getDrawing(drawingTimestampID)).to.equal(attribute);
         done();
     });
 
     it('#getTags should return tags', function(done) {
         const attribute = this.getTags();
-        expect(this.getTags()).to.return(attribute);
+        expect(this.getTags()).to.eql(attribute);
         done();
     });
 
     it('#dateToId should change date into id', function(done) {
         const date = '00/00/00';
-        expect(this.dateToId(date)).to.return('000000');
+        expect(this.dateToId(date)).to.eql('000000');
         done();
     });
 });
