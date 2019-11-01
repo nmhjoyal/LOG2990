85e701f284efe0b1330c385802c6b392bbaf38ff
Persia <persiashahdi@gmail.com>
2019-11-01 13:42:33 -0400

    PS - implemented redOutline method that outlines shapes in red on hover, if eraserSelected

--- client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, HostListener } from '@angular/core';
 import ClickHelper from '../../../../../helpers/click-helper/click-helper';
 import { SelectorService } from '../../../../../services/selector-service/selector-service';
 import { ToolHandlerService } from '../../../../../services/tool-handler/tool-handler.service';
+import { IShape } from '../../assets/interfaces/shape-interface';
+import { ColorService } from 'src/app/services/color_service/color.service';
 
 @Component({
   selector: 'app-eraser',
@@ -10,7 +12,7 @@ import { ToolHandlerService } from '../../../../../services/tool-handler/tool-ha
 })
 export class EraserComponent {
 
-  constructor(private toolService: ToolHandlerService, private selectorService: SelectorService) {
+  constructor(private toolService: ToolHandlerService, private selectorService: SelectorService, private colorService: ColorService) {
   }
 
   @Input() windowHeight: number;
@@ -29,6 +31,19 @@ export class EraserComponent {
     }
   }
 
+  redOutline(event: MouseEvent): void {
+    for (let i = this.toolService.drawings.length - 1; i >= 0; i--) {
+      const drawing = this.toolService.drawings[i] as IShape;
+
+      if (this.selectorService.cursorTouchesObject(drawing, ClickHelper.getXPosition(event), ClickHelper.getYPosition(event))) {
+        drawing.secondaryColor = 'red';
+      } else {
+        drawing.secondaryColor = this.colorService.color[1];
+      }
+
+    }
+  }
+
   @HostListener('mousedown', ['$event']) mouseDown(event: MouseEvent): void {
     this.leftClicked = true;
     this.eraseObject(event);
@@ -39,6 +54,8 @@ export class EraserComponent {
   }
 
   @HostListener('mousemove', ['$event']) mouseMove(event: MouseEvent): void {
+    this.redOutline(event);
+
     if (this.leftClicked) {
       this.eraseObject(event);
     }
