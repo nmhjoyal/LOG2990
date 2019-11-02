a69194e1a13f183b6ec850cd244055bb07e6d2e4
Persia <persiashahdi@gmail.com>
2019-11-02 13:46:40 -0400

    Fixed issue where all stacked objects would get a red outline on hover using a boolean and slightly different structure

--- client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/eraser/eraser.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, HostListener } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
+import { ColorService } from 'src/app/services/color_service/color.service';
 import ClickHelper from '../../../../../helpers/click-helper/click-helper';
 import { SelectorService } from '../../../../../services/selector-service/selector-service';
 import { ToolHandlerService } from '../../../../../services/tool-handler/tool-handler.service';
 import { IShape } from '../../assets/interfaces/shape-interface';
-import { ColorService } from 'src/app/services/color_service/color.service';
 
 @Component({
   selector: 'app-eraser',
@@ -18,6 +18,8 @@ export class EraserComponent {
   @Input() windowHeight: number;
   @Input() windowWidth: number;
   leftClicked: boolean;
+  userInputWidth: number;
+  userInputHeight: number;
 
   eraseObject(event: MouseEvent): void {
     for (let i = this.toolService.drawings.length - 1; i >= 0; i--) {
@@ -32,15 +34,21 @@ export class EraserComponent {
   }
 
   redOutline(event: MouseEvent): void {
+
+    // As soon as the cursor hovers on an object, this is set false to exit 'if' to prevent multiple red outlines
+    let touchedFirstObject = true;
+
     for (let i = this.toolService.drawings.length - 1; i >= 0; i--) {
       const drawing = this.toolService.drawings[i] as IShape;
-
-      if (this.selectorService.cursorTouchesObject(drawing, ClickHelper.getXPosition(event), ClickHelper.getYPosition(event))) {
-        drawing.secondaryColor = 'red';
-      } else {
+      if (drawing.secondaryColor === 'red') {
         drawing.secondaryColor = this.colorService.color[1];
       }
 
+      if (this.selectorService.cursorTouchesObject(drawing, ClickHelper.getXPosition(event), ClickHelper.getYPosition(event))
+       && touchedFirstObject) {
+        drawing.secondaryColor = 'red';
+        touchedFirstObject = false;
+      }
     }
   }
 
