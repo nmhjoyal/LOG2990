6925959183e23324aae4d065aead417e697a8d7a
unknown <persiashahdi@gmail.com>
2019-11-05 23:36:22 -0500

    PS - Added eraser to drawing-tool-toolbox

--- client/src/app/drawing-view/components/tools/drawing-tools/drawing-tool-toolbox/drawing-tool-toolbox.component.ts
+++ client/src/app/drawing-view/components/tools/drawing-tools/drawing-tool-toolbox/drawing-tool-toolbox.component.ts
@@ -13,6 +13,7 @@ export class DrawingToolToolboxComponent implements OnInit {
 
   mode = ModeType;
   pointMode = PointType;
+  eraserSizeInput = 10;
   @Input() canvas: CanvasComponent;
 
   constructor(protected toolHandler: ToolHandlerService) {
